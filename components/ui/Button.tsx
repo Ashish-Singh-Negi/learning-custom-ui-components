1b145fc43b24cae06cc72462fe942df9eeaeda21
Ashish Singh Negi <ashishamonegi@gmail.com>
2026-02-14 15:32:31 +0530

    init

---
+++ components/ui/Button.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+type ButtonProps = {
+  variant?: "primary" | "secondary" | "outline";
+  size?: "sm" | "md" | "lg";
+  disabled?: boolean;
+};
+
+const Button = () => {
+  return <div>Button</div>;
+};
+export default Button;
