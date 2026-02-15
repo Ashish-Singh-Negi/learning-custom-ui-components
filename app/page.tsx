ee137a827164a1a99e2a481d05892fb1d698558f
Ashish Singh Negi <ashishamonegi@gmail.com>
2026-02-15 20:20:46 +0530

    add: another btn

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Alert from "@/components/ui/Alert";
 import Badge from "@/components/ui/Badge";
 import Button from "@/components/ui/Button";
+import Button2 from "@/components/ui/Button2";
 
 export default function Home() {
   return (
-    <main className="h-dvh w-screen py-10 flex flex-col gap-10 px-10 bg-black">
+    <main className="h-dvh w-screen py-10 flex flex-col gap-10 px-10">
       <div className="flex gap-4">
         <Button variant={"primary"} size={"md"} />
         <Button variant={"secondry"} size={"md"} />
@@ -21,6 +22,15 @@ export default function Home() {
         <Alert type={"success"} alertMessage="Are you sure!!" />
         <Alert type={"warning"} alertMessage="Are you sure!!" />
         <Alert type={"error"} alertMessage="Are you sure!!" withIcon />
+      </div>
+      <div className="w-full flex gap-4">
+        <Button2 children={"primary"} size={"sm"} />
+        <Button2 children={"primary"} />
+        <Button2 children={"primary"} size={"lg"} loading />
+        <Button2 children={"secondary"} variant={"secondary"} loading />
+        <Button2 children={"ghost"} variant={"ghost"} />
+        <Button2 children={"outline"} variant={"outline"} />
+        <Button2 children={"ghost"} variant={"ghost"} fullWidth />
       </div>
     </main>
   );
---
+++ components/ui/Button2.tsx
@@ -0,0 +1,74 @@
+import { cn } from "@/lib/utils";
+import { cva, VariantProps } from "class-variance-authority";
+import { ButtonHTMLAttributes } from "react";
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center font-medium transition-all active:scale-95 focus:outline-none disabled:cursor-not-allowed disabled:opacity-70 cursor-pointer",
+  {
+    variants: {
+      variant: {
+        primary: "bg-black text-white hover:bg-gray-900",
+        secondary:
+          "bg-white text-black border border-black hover:bg-black hover:text-white",
+        outline:
+          "border border-black text-black hover:bg-black hover:text-white",
+        ghost: "bg-gray-50 text-black hover:bg-gray-100",
+      },
+
+      size: {
+        sm: "h-8 px-3 text-sm rounded-sm",
+        md: "h-10 px-4 text-base rounded-md",
+        lg: "h-12 px-6 text-lg rounded-lg",
+      },
+
+      loading: {
+        true: "pointer-events-none opacity-70",
+        false: "",
+      },
+
+      fullWidth: {
+        true: "w-full",
+        false: "",
+      },
+    },
+
+    compoundVariants: [
+      {
+        variant: "ghost",
+        loading: true,
+        class: "hover:bg-transparent",
+      },
+    ],
+
+    defaultVariants: {
+      variant: "primary",
+      size: "md",
+    },
+  },
+);
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> &
+  VariantProps<typeof buttonVariants>;
+
+export default function Button2({
+  className,
+  variant,
+  size,
+  loading,
+  fullWidth,
+  children,
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      disabled={loading!}
+      className={cn(
+        buttonVariants({ variant, size, loading, fullWidth }),
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
